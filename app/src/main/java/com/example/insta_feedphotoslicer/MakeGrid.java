06f3c592870c38c1f3b33ddfdd91c5648b55cead
Rose Mary <rosemary123c@gmail.com>
2021-04-18 11:08:29 +0530

    commit

--- app/src/main/java/com/example/insta_feedphotoslicer/MakeGrid.java
+++ app/src/main/java/com/example/insta_feedphotoslicer/MakeGrid.java
@@ -2,13 +2,30 @@ package com.example.insta_feedphotoslicer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MakeGrid extends AppCompatActivity {
+public class MakeGrid extends AppCompatActivity implements View.OnClickListener {
 
+    Button another;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_grid);
+
+        another= findViewById(R.id.another);
+        another.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.another) {
+            Intent I = new Intent(MakeGrid.this, HomePage.class);
+            startActivity(I);
+        }
+
     }
 }
